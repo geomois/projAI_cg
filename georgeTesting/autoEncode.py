9d3091efb8c728bc53c0d86d84bb6df395e5c5fc
geomois <geomois@gmail.com>
2016-06-18 21:35:05 +0200

    save to wav instead of pickle. downsample to 30%, mono sound (1 channel)

--- georgeTesting/autoEncode.py
+++ georgeTesting/autoEncode.py
@@ -1,184 +1,204 @@
-#call: python <scriptName> <trainFiles directory> <annotations directory> <read> or <write> or <ogg> or <simple> <number of files to read>
+# call: python <scriptName> <trainFiles directory> <annotations directory> <read> or <write> or <wav> or <simple> <number of files to read>
 import numpy as np
 import soundfile as sf
 import os
 import sys
-from model import kerasModel
+# from model import kerasModel
 import scipy.signal as signal
 import pickle
 from toyPipeline import MyAudio
 import gzip
 
-def prepareAudio(directory,size=1):
-    oggs=[]
+
+def prepareAudio(directory, size=1):
+    oggs = []
     for subdir, dirs, files in os.walk(directory):
         for f in files:
-            if f.endswith('.ogg'):
-            # if (f.endswith('.ogg') or f.endswith('.mp3')):
-            # lib soundfile doesn't recognize .mp3 files
-                oggs.append(os.path.join(subdir,f))
-    
-    oggs=sorted(oggs)
-    trainWaves=[]
-    trainRates=[]
-    if size>len(oggs):
-        stop=len(oggs)
+            if f.endswith('.ogg') or f.endswith('.wav'):
+                # if (f.endswith('.ogg') or f.endswith('.mp3')):
+                # lib soundfile doesn't recognize .mp3 files
+                oggs.append(os.path.join(subdir, f))
+
+    oggs = sorted(oggs)
+    trainWaves = []
+    trainRates = []
+    if size > len(oggs):
+        stop = len(oggs)
     else:
-        stop=size
-        
+        stop = size
+
     if (len(oggs) > 0):
         count = 0
         for path in oggs:
-            audioTemp,rateTemp=sf.read(path)
+            audioTemp, rateTemp = sf.read(path)
             trainWaves.append(audioTemp)
-            count+=1
-	    trainRates.append(rateTemp)
-            if count==stop:
-                break
+            count += 1
+            trainRates.append(rateTemp)
             # In case we want to save the wave files, uncomment the following line
-            # sf.write(os.path.dirname(path)+"/"+os.path.basename(path).split('.')[0]+'.wav',audioTemp,rateTemp)
-    return trainWaves,trainRates,oggs
+            # sf.write(os.path.dirname(path) + "/" + os.path.basename(path).split('.')[0] + '.wav', audioTemp, rateTemp)
+            if count == stop:
+                break
+    return trainWaves, trainRates, oggs
 
-def readAnnotations(directory,audioPaths):
-#    fileNames=[]
-    timings=[]
+
+def readAnnotations(directory, audioPaths):
+    #    fileNames=[]
+    timings = []
     for path in audioPaths:
-        timings.append(os.path.dirname(directory)+'/'+os.path.basename(path).split('.')[0]+'.lab')
-#	print os.path.basename(os.path.dirname(directory)+'/'+os.path.basename(path).split('.')[0]+'.lab')
-    annotations=[]
+        timings.append(os.path.dirname(directory) + '/' + os.path.basename(path).split('.')[0] + '.lab')
+    #	print os.path.basename(os.path.dirname(directory)+'/'+os.path.basename(path).split('.')[0]+'.lab')
+    annotations = []
     for path in timings:
-        with open(path,'r') as aFile:
-            content=aFile.read().splitlines()
-            l=[]            
+        with open(path, 'r') as aFile:
+            content = aFile.read().splitlines()
+            l = []
             for line in content:
-                temp=line.split(' ')
-                l.append([float(temp[0]), float(temp[1]),(True if temp[2]=='sing' else False)])
+                temp = line.split(' ')
+                l.append([float(temp[0]), float(temp[1]), (True if temp[2] == 'sing' else False)])
             annotations.append(np.asarray(l))
-    
+
     return annotations
 
-def downSample(waves,rate):
-#   Downsample waveform by percentage of the original signal, fourier transformation
-    percentage=50
-    resampledSignals=[]
-    resampledRates=[]
-    for i in range(0,len(waves)):
-        newRate=(rate[i]*percentage)/100
-        temp=np.asarray(signal.resample(waves[i],(len(waves[i])/rate[i])*newRate))
-        resampledSignals.append(temp)
-        print 'resampled: ',i
-        resampledRates.append(newRate)
- 
-    return resampledSignals,resampledRates
+def toMono(waves):
+    mono=[]
+    for i in range(len(waves)):
+        mono.append(np.sum(waves[i], 1)/2.0)
+    print 'mono ', mono[0].shape
+    return mono
 
-    
-def prepareAnnotations(signals,rate,annotations):
-    aWaves=[]
-    for k in range(0,len(signals)):
-        aWaveTemp=np.zeros((1,len(signals[k])))
-        for j in range(0,len(annotations[k])):
-		 if(annotations[k][j][2]):
-			start=np.ceil(rate[k]*annotations[k][j][0])
-                        end=np.floor(rate[k]*annotations[k][j][1])
-                        aWaveTemp[0][start:end]=[1 for u in range(int(start),int(end))]
+
+def downSample(waves, rate):
+    #   Downsample waveform by percentage of the original signal, fourier transformation
+    percentage = 30
+    resampledSignals = []
+    resampledRates = []
+    for i in range(0, len(waves)):
+        newRate = (rate[i] * percentage) / 100
+        temp = np.asarray(signal.resample(waves[i], (len(waves[i]) / rate[i]) * newRate))
+        resampledSignals.append(temp)
+        print 'resampled: ', i
+        resampledRates.append(newRate)
+
+    return resampledSignals, resampledRates
+
+
+def prepareAnnotations(signals, rate, annotations):
+    aWaves = []
+    for k in range(0, len(signals)):
+        aWaveTemp = np.zeros((1, len(signals[k])))
+        for j in range(0, len(annotations[k])):
+            if (annotations[k][j][2]):
+                start = np.ceil(rate[k] * annotations[k][j][0])
+                end = np.floor(rate[k] * annotations[k][j][1])
+                aWaveTemp[0][start:end] = [1 for u in range(int(start), int(end))]
         aWaves.append(aWaveTemp[0])
-    
+
     return aWaves
 
-def toPickle(writeFlag,waves=None,rates=None,annotation=None):
+
+def toPickle(writeFlag, waves=None, rates=None, annotation=None):
     if writeFlag:
         if annotation != None:
-            f = gzip.open('/local/gms590/pickled/annot.pi.pklz','wb')
-            pickle.dump(annotation,f)
+            f = gzip.open('/local/gms590/pickled/annot.pi.pklz', 'wb')
+            pickle.dump(annotation, f)
             f.close()
         if waves != None:
-            f = gzip.open('/local/gms590/pickled/wav.pi.pklz','wb')
-            pickle.dump(waves,f)
+            f = gzip.open('/local/gms590/pickled/wav.pi.pklz', 'wb')
+            pickle.dump(waves, f)
             f.close()
         if rates != None:
-            f=gzip.open('/local/gms590/pickled/rates.pi.pklz','wb')
-            pickle.dump(rates,open('/local/gms590/pickled/rates.pi','wb'))
+            f = gzip.open('/local/gms590/pickled/rates.pi.pklz', 'wb')
+            pickle.dump(rates, open('/local/gms590/pickled/rates.pi', 'wb'))
             f.close()
     else:
-        if waves != None:
-            f=gzip.open('/local/gms590/pickled/wav.pi.pklz','rb')
-            waves=pickle.load(f)
+        if waves is not None:
+            f = gzip.open('/local/gms590/pickled/wav.pi.pklz', 'rb')
+            waves = pickle.load(f)
             f.close()
-            waves= pickle.load(open("/local/gms590/pickled/wav.pi","rb"))
+            waves = pickle.load(open("/local/gms590/pickled/wav.pi", "rb"))
         if annotation != None:
-            f=gzip.open('/local/gms590/pickled/annot.pi.pklz','rb')
-            annotation=pickle.load(f)
+            f = gzip.open('/local/gms590/pickled/annot.pi.pklz', 'rb')
+            annotation = pickle.load(f)
             f.close()
-            annotation=pickle.load(open('/local/gms590/pickled/annot.pi','rb'))
+            annotation = pickle.load(open('/local/gms590/pickled/annot.pi', 'rb'))
         if rates != None:
-            f=gzip.open('/local/gms590/pickled/rates.pi.pklz','rb')
-            rates=pickle.load(f)
+            f = gzip.open('/local/gms590/pickled/rates.pi.pklz', 'rb')
+            rates = pickle.load(f)
             f.close()
-            
-        return waves,annotation,rates        
 
+        return waves, annotation, rates
 
-def toOgg(waves,rates,paths):
+def toWav(waves, rates, paths):
     print 'toOgg'
-    for i in range(0,len(waves)):
-        sf.write('/local/gms590/resampled/'+ os.path.basename(paths[i]),waves[i][:,:],rates[i])
+    for i in range(0, len(waves)):
+        sf.write('../resampled/'+os.path.basename(paths[i]).split('.')[0]+'.wav', waves[i], rates[i])
 
 if __name__ == '__main__':
-#    simpleRun=True
-
-    simpleRun=False
-    if sys.argv[3] =='read':
-        waves,annotation,rates = toPickle(False,1,1,1)
-    elif sys.argv[3] =='write':
-        waves,rate,paths=prepareAudio(sys.argv[1]),int(sys.argv[4])
-        annotations=readAnnotations(sys.argv[2],paths)
-        signals,downRate=downSample(waves,rate)
-        annotationWave=prepareAnnotations(signals,downRate,annotations) 
-        toPickle(True,signals,downRate,annotationWave)
-    elif sys.argv[3] == 'ogg':
-        waves,rate,paths=prepareAudio(sys.argv[1])
-        annotations=readAnnotations(sys.argv[2],paths)
-        signals,downRate=downSample(waves,rate)
-        annotationWave=prepareAnnotations(signals,downRate,annotations)
-        toOgg(signals,downRate,paths)
+    simpleRun = False
+    if sys.argv[3] == 'read':
+        # waves, annotation, foo = toPickle(False, None, None, 1)
+        signals,downRate,paths=prepareAudio('../resampled/', 100)
+        annotations = readAnnotations(sys.argv[2], paths)
+        annotationWave=prepareAnnotations(signals, downRate, annotations)
+    elif sys.argv[3] == 'write':
+        waves, rate, paths = prepareAudio(sys.argv[1], int(sys.argv[4]))
+        annotations = readAnnotations(sys.argv[2], paths)
+        signals, downRate = downSample(waves, rate)
+        annotationWave = prepareAnnotations(signals, downRate, annotations)
+        toPickle(True, signals, downRate, annotationWave)
+    elif sys.argv[3] == 'wav':
+        waves, rate, paths = prepareAudio(sys.argv[1],int(sys.argv[4]))
+        annotations = readAnnotations(sys.argv[2], paths)
+        waves = toMono(waves)
+        signals, downRate = downSample(waves, rate)
+        annotationWave = prepareAnnotations(signals, downRate, annotations)
+        toWav(signals, downRate, paths)
+        print 'done saving'
     else:
-        simpleRun=True
-         
+        simpleRun = True
+
     if simpleRun:
         print 'simpleRun'
-        waves,rate,paths=prepareAudio(sys.argv[1],int(sys.argv[4]))
-        annotations=readAnnotations(sys.argv[2],paths)
-        signals,downRate=downSample(waves,rate)
-        annotationWave=prepareAnnotations(signals,downRate,annotations)
+        waves, rate, paths = prepareAudio(sys.argv[1], int(sys.argv[4]))
+        annotations = readAnnotations(sys.argv[2], paths)
+        signals, downRate = downSample(waves, rate)
+        annotationWave = prepareAnnotations(signals, downRate, annotations)
 
-#    waves,rate,paths=prepareAudio("/home/george/Desktop/Project AI/projGit/Annotated_music/train/")
-#    annotations=readAnnotations("/home/george/Desktop/Project AI/projGit/Annotated_music/jamendo_lab/",paths)
-#    signals,downRate=downSample(waves,rate)
-#    annotationWave=prepareAnnotations(signals,downRate,annotations)
-#     
-    #for one audio (i.e. signal in class autoEncode.py) call:
-    endFile=[] # 
-    test=MyAudio(downRate[0],signals[0],1,annotationWave[0])
+    # waves, rate, paths = prepareAudio("/home/iam/PycharmProjects/projAI_cg/Annotated_music/train")
+    # annotations = readAnnotations("/home/iam/PycharmProjects/projAI_cg/Annotated_music/jamendo_lab/", paths)
+    # waves=toMono(waves)
+    # signals, downRate = downSample(waves, rate)
+    # annotationWave = prepareAnnotations(signals, downRate, annotations)
+    # toWav(signals, downRate, paths)
+    # sys.exit()
+
+    #    waves,rate,paths=prepareAudio("/home/george/Desktop/Project AI/projGit/Annotated_music/train/")
+    #    annotations=readAnnotations("/home/george/Desktop/Project AI/projGit/Annotated_music/jamendo_lab/",paths)
+    #    signals,downRate=downSample(waves,rate)
+    #    annotationWave=prepareAnnotations(signals,downRate,annotations)
+    #
+    # for one audio (i.e. signal in class autoEncode.py) call:
+
+    endFile=[]
+    test = MyAudio(downRate[0], signals[0], 1, annotationWave[0])
     test.split()
-    inp,out=test.getInputOutputMatrices()
-    test_input_matrix=inp
-    test_output_matrix=out
+    inp, out = test.getInputOutputMatrices()
+    test_input_matrix = inp
+    test_output_matrix = out
     endFile.append(len(test_output_matrix))
-    for i in range(1,len(signals)):   
-        test=MyAudio(downRate[i],signals[i],1,annotationWave[i])
+    for i in range(1, len(signals)):
+        test = MyAudio(downRate[i], signals[i], 1, annotationWave[i])
         test.split()
-        inp,out=test.getInputOutputMatrices()
-        test_input_matrix=np.hstack((test_input_matrix,inp))
-        test_output_matrix=np.vstack((test_output_matrix,out))
+        inp, out = test.getInputOutputMatrices()
+        test_input_matrix = np.hstack((test_input_matrix, inp))
+        test_output_matrix = np.vstack((test_output_matrix, out))
         endFile.append(len(test_output_matrix))
-        
-    
-    outArray=test_output_matrix.reshape((test_output_matrix.shape[0],test_output_matrix.shape[1],1))
-    inArray=test_input_matrix[0].reshape((test_input_matrix[0].shape[0],test_input_matrix[0].shape[1],1))
+
+    outArray = test_output_matrix.reshape((test_output_matrix.shape[0], test_output_matrix.shape[1], 1))
+    inArray = test_input_matrix[0].reshape((test_input_matrix[0].shape[0], test_input_matrix[0].shape[1], 1))
 
     print "outArray ", outArray.shape
     print "inArray ", inArray.shape
 
-    m=kerasModel(inArray,downRate,outArray)
-    m.buildAutoEncoder(True)
-
+    # m = kerasModel(inArray, downRate, outArray)
+    # m.buildAutoEncoder(True)
--- georgeTesting/toyPipeline.py
+++ georgeTesting/toyPipeline.py
@@ -24,7 +24,7 @@ class MyAudio:
 		
         	c=1
 		self.chunks=[] #chunks(seconds) in 1st channel
-		self.chunks2=[] #chunks in 2nd channel
+		# self.chunks2=[] #chunks in 2nd channel
 		self.chunks3=[] #chunks in annotations
 
         	for start_pos in range(0,self.audio.shape[0],samples_in_chunk):
@@ -35,33 +35,33 @@ class MyAudio:
                         	end_pos = self.audio.shape[0]- 1
 #				print "now ",end_pos 
                		#print "start_pos, end_pos:",start_pos, " ",end_pos
-			chunk = self.audio[start_pos : end_pos,0]
-			chunk2 = self.audio[start_pos : end_pos,1]
+			chunk = self.audio[start_pos : end_pos]
+			# chunk2 = self.audio[start_pos : end_pos,1]
 			chunk3 = self.annotation[start_pos : end_pos]
 #			print "chunk length ",len(chunk)
                 	if len(chunk)<samples_in_chunk:
 				chunk=np.append(chunk,np.zeros(samples_in_chunk-len(chunk)))
 #				print "now ch",len(chunk)
-				chunk2=np.append(chunk2,np.zeros(samples_in_chunk-len(chunk2)))
+				# chunk2=np.append(chunk2,np.zeros(samples_in_chunk-len(chunk2)))
 				chunk3=np.append(chunk3,np.zeros(samples_in_chunk-len(chunk3)))
                         	#print "append some zeros ",len(chunk)
 #			print "chunk lengths:",chunk.shape,chunk2.shape,chunk3.shape
                 	self.chunks.append(chunk)
-			self.chunks2.append(chunk2)
+			# self.chunks2.append(chunk2)
 			self.chunks3.append(chunk3)
 #		print "chunk lengths:",len(self.chunks),len(self.chunks2),len(self.chunks3)
 
 	def getInputOutputMatrices(self):
 		matrix_width=len(self.chunks[0])#afterwards we can resize according for pooling
 		matrix_height=len(self.chunks)
-		numchannels=2
+		numchannels=1
 #		print "matrix_width",matrix_width
 #		print "matrix_height",matrix_height
 		self.input_matrix=np.zeros(shape=(numchannels,matrix_height,matrix_width))
 		self.output_matrix=np.zeros(shape=(matrix_height,matrix_width))
         	for i,chunk in enumerate(self.chunks):
 	                self.input_matrix[0,i,:]=self.chunks[i][0:matrix_width]
-			self.input_matrix[1,i,:]=self.chunks2[i][0:matrix_width]
+			# self.input_matrix[1,i,:]=self.chunks2[i][0:matrix_width]
        			self.output_matrix[i,:]=self.chunks3[i][0:matrix_width]
 #		print"chunks[i][0:matrix_width]:",self.chunks[2][0:matrix_width].shape
 #		print"matrix_width",matrix_width
