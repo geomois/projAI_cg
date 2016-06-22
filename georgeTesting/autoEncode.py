06941f0273029ee9f14befe3081fca0b040cce6f
Christina Zavou <czavou@node402.cm.cluster>
2016-06-22 18:26:11 +0200

    accuracy

--- georgeTesting/autoEncode.py
+++ georgeTesting/autoEncode.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
     print 'sigValid ', sigValid.shape
     batch=100
     
-    #m = kerasModel(sigArray,annotArray,sigValid,annotValid)    
+    m = kerasModel(sigArray,annotArray,sigValid,annotValid)    
     """
     m.buildAutoEncoder(32,2,'mean_squared_error','adadelta','relu')
     m.autoEncoderTrain(annotArray,10,128,'ae_weights.w_relu')    
@@ -231,9 +231,13 @@ if __name__ == '__main__':
     m.buildAutoEncoder(32,128,'mean_squared_error','adadelta','relu')
     m.autoEncoderTrain(annotArray,25,128,'ae_weights_256len128.w')
     """
-    #m.loadWeights('ae_weights_len100_elu.w')
-    from kModel import kModel
-    m=kModel(sigArray,0,annotArray,sigValid,annotValid,0)
-    from keras import backend as K
-    m.buildAutoEncoder(1,K.placeholder((1,sigArray.shape[1],1)))
-    
+    """
+    m.buildAutoEncoder(64,128,'mean_squared_error','adadelta','relu')
+    #m.autoEncoderTrain(annotArray,25,128,'aw_weights_64_8len128_adadelta_relu.w')
+    m.loadWeights('aw_weights_64_8len128_adadelta_relu.w')
+    m.evaluate()    
+    """
+    m.buildAutoEncoder(32,2)
+    m.autoEncoderTrain(annotArray,20,128,'weights32_2_bce_adadelta_relu.w')
+    #m.loadWeights('weights32_2_bce_adadelta_relu.w')
+    m.evaluate() #gives 50% accuracy
--- georgeTesting/model.py
+++ georgeTesting/model.py
@@ -16,9 +16,13 @@ class kerasModel:
         self.autoencoder=None
 	print "kkk",len(self.signals[1])
 
-    def buildAutoEncoder(self,init_filter=32,filter_size=2,loss_f='mean_squared_error',optim_f='adadelta',f='relu'):
+    def buildAutoEncoder(self,init_filter=32,filter_size=2,loss_f='binary_crossentropy',optim_f='adadelta',f='relu'):
         input_au=Input(shape=(self.signals.shape[1], 1))
 
+	if filter_size==64:	
+	        x = Convolution1D(init_filter/8, filter_size, activation=f, border_mode='same')(input_au)#filter/4*shape
+	        x = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)#filter/4*shape/2
+ 
         x = Convolution1D(init_filter/4, filter_size, activation=f, border_mode='same')(input_au)#filter/4*shape
         x = AveragePooling1D(pool_length=2, stride=None, border_mode="valid")(x)#filter/4*shape/2
         x = Convolution1D(init_filter/2, filter_size, activation=f, border_mode='same')(x)#filter/2*shape/2
@@ -29,22 +33,40 @@ class kerasModel:
         x = UpSampling1D(length=2)(x)#filter/2*shape/2
         x = Convolution1D(init_filter/4, filter_size, activation=f, border_mode='same')(x)#filter/4*shape/2
 	x = UpSampling1D(length=2)(x)#filter/4*shape
+
+	if filter_size==64:
+	       x = Convolution1D(init_filter/8, filter_size, activation=f, border_mode='same')(x)#filter/4*shape/2
+ 	       x = UpSampling1D(length=2)(x)#filter/4*shape
+
         x = Convolution1D(1, filter_size, activation=f,border_mode='same')(x)#1*shape
 
         self.autoencoder = Model(input_au, x)
         self.autoencoder.compile(optimizer=optim_f, loss=loss_f)
+
     def autoEncoderTrain(self,target=None,epochs=20,batch=128,name='ae_weights.w'):
     	if target is None:
             target=self.signals
         print 'Training..'
 	target=target[:,0:13208]
 	#history=callbacks.History()
-	#earlystop=callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+	earlystop=callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
         #checkpoint=callbacks.ModelCheckpoint(name, monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
 	print("in train",self.signals.shape,target.shape)
-	self.autoencoder.fit(self.signals, target, nb_epoch=epochs, batch_size=batch, shuffle=True, callbacks=[],validation_data=(self.validation, self.validAnnotation))	
+	self.autoencoder.fit(self.signals, target, nb_epoch=epochs, batch_size=batch, shuffle=True, callbacks=[earlystop], validation_data=(self.validation, self.validAnnotation))	
         self.autoencoder.save_weights(name, True)
 	
     def loadWeights(self,name):
 	self.autoencoder.load_weights(name)
     
+    def evaluate(self):
+	print "accuracy ",self.autoencoder.evaluate(self.validation, self.validAnnotation)
+	pred=self.autoencoder.predict(self.validation)
+	print pred.shape
+	print pred[0:2]
+	pred[pred>=0.5]=1
+	pred[pred<0.5]=0
+	print pred[0:2]
+	diff=self.validAnnotation-pred	
+	print diff[0:2]
+	acc=np.sum(np.abs(diff))
+	print "accuracy ",acc/(diff.shape[0]*diff.shape[1]) 
