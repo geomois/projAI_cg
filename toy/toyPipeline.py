ac7ce2ae557fc6c6386f899cb7e952f60b9c2ead
Christina Zavou <czavou@node402.cm.cluster>
2016-06-22 19:28:39 +0200

    sigirizo

---
+++ toy/toyPipeline.py
@@ -0,0 +1,94 @@
+import numpy as np
+from scipy.io import wavfile
+from scipy import signal
+from matplotlib import pyplot as plt
+
+class MyAudio:
+
+	def __init__(self,audio_sample_freq,audio""",filename,maxFreq,"""chunk_duration):
+		#self.audio_sample_freq, self.audio = wavfile.read('toy_data_sines_44_1khz.wav')
+		self.audio_sample_freq=audio_sample_freq
+		self.audio=audio
+		#print "audio sample freq ", self.audio_sample_freq
+
+		#self.audio_duration=self.audio.shape[0]/self.audio_sample_freq
+		self.audio_duration=self.audio.shape[0]/self.audio_sample_freq
+		print "audio duration ", self.audio_duration#, " real duration ", str(984)
+
+		#self.maxFreq=440
+		self.chunk_duration=1 # in seconds
+		print "arxika audio shape ", audio.shape
+"""
+	def downsample(self):
+        	double_b=self.maxFreq*2
+        	#the audio has 441000 samples per sec but we want to downsample
+        	#i.e. to have 880 samples per sec so we will decimate every 441000/880
+
+        	#test how many channels
+        	print "audio shape ", self.audio.shape
+        	step=self.audio_sample_freq/double_b
+        	print "step ", step
+        	self.downsampled_audio=signal.decimate(self.audio,step)
+        	return self.downsampled_audio,step
+"""
+
+	def split(self""",write_files"""):
+        	#samples_in_chunk=self.downsampled_audio.shape[0]/self.audio_duration
+        	samples_in_chunk=self.audio_sample_freq#WE NOW RECEIVE THE ALREADY DOWNSAMPLED(and since we split in 1 sec it's that rate)
+		print "samples in chunk ", samples_in_chunk
+
+        	c=1
+		self.chunks=[]
+		self.chunks2=[]
+		
+        	for start_pos in range(0,self.audio.shape[0]"""self.downsampled_audio.shape[0]""",samples_in_chunk):
+                	end_pos = start_pos + samples_in_chunk
+                	if end_pos >= self.audio.shape[0]"""len(self.downsampled_audio)""":
+                        	end_pos = self.audio.shape[0]"""len(self.downsampled_audio)""" - 1
+               		#print "start_pos, end_pos:",start_pos, " ",end_pos
+                	chunk = """self.downsampled_audio"""self.audio[start_pos : end_pos,0]
+			chunk2 = self.audio[start_pos : end_pos,1]
+                	if chunk.shape[0]<samples_in_chunk:
+                        	chunk=np.append(chunk,np.zeros(samples_in_chunk-len(chunk)))#make for channels
+				chunk2=np.append(chunk2,np.zeros(samples_in_chunk-len(chunk2)))
+                        	#print "append some zeros ",len(chunk)
+                	"""
+			if write_files==True:
+                        	wavfile.write("created/"+str(c)+".wav",samples_in_chunk/chunk_duration, chunk)
+			c = c + 1
+                	#print "wrote file: ", str(c)+".wav"
+			"""
+                	self.chunks.append(chunk)
+			self.chunks2.append(chunk2)
+			
+		toreturn=np.vstack((np.array(self.chunks),np.array(self.chunks2)))
+		print "to return chunks shape ", toreturn.shape
+        	#return self.chunks
+
+	def getInputMatrix(self):
+	        #matrix_width=len(self.chunks[0]) #this is 882 but we ll use 880
+		#matrix_width=880
+
+		matrix_width=len(self.chunks[0])#afterwards we can resize according for pooling
+		matrix_height=len(self.chunks)
+		numchannels=2
+		self.input_matrix=np.zeros(shape=(matrix_height,matrix_width,numchannels))
+        	for i,chunk in enumerate(self.chunks):
+	                self.input_matrix[i,:,0]=self.chunks[i][0:matrix_width]
+			self.input_matrix[i,:,1]=self.chunks2[i][0:matrix_width]
+       		print "inp matr shap ",self.input_matrix.shape
+        	return self.input_matrix
+
+#pipeline:
+"""
+test=MyAudio('toy_data_sines_44_1khz.wav',440,1)
+downsampled_test,sample_freq=test.downsample()
+print "downsampled_audio shape,freq ",downsampled_test.shape, " ",sample_freq
+#print "sampled audio duration ", downsampled_audio.shape[0]/sample_freq, " BAD"
+audio_chunks_of1sec = test.split(False)
+input_matrix=test.getInputMatrix()
+"""
+#for one audio (i.e. signal in class autoEncode.py) call:
+test=MyAudio(downRate,signal,1)
+testchunks=test.split()
+testmatrix=test.getInputMatrix()
---
+++ toy/toy_autoencoder_CNN.py
@@ -0,0 +1,55 @@
+
+from keras.layers import Convolution1D, UpSampling1D, AveragePooling1D, MaxPooling1D
+from keras.models import Sequential
+from keras.layers import Input,Dense
+from keras.models import Model
+from keras import regularizers
+from keras.optimizers import SGD
+from scipy.io import wavfile
+import numpy as np
+from sklearn.metrics import mean_squared_error
+import os
+import matplotlib.pyplot as plt
+
+from toyPipeline import MyAudio
+
+data=MyAudio('toy_data_sines_44_1khz.wav',440,1)
+downsampled_data,sample_freq=data.downsample()
+audio_chunks_of1sec = data.split(False)
+data_input=data.getInputMatrix()
+print "NOW:",data_input.shape
+
+
+input_img = Input(shape=(880,1))
+
+x = Convolution1D(16, 40, activation='relu', border_mode='same')(input_img)
+x = MaxPooling1D(2, border_mode='valid')(x) #outvol=440x16
+x = Convolution1D(8,20, activation='relu', border_mode='same')(x)
+x = MaxPooling1D(2, border_mode='valid')(x) #outvol=220x8
+x = Convolution1D(4,10, activation='relu', border_mode='same')(x)
+encoded = MaxPooling1D(2, border_mode='valid')(x) #outvol=110x4
+
+# at this point the representation is (110, 4) i.e. 440-dimensional
+
+x = Convolution1D(4,10, activation='relu', border_mode='same')(encoded)
+x = UpSampling1D(2)(x) #outvol=220x4
+x = Convolution1D(8,20, activation='relu', border_mode='same')(x)
+x = UpSampling1D(2)(x) #outvol=440x8
+x = Convolution1D(16,40, activation='relu')(x)
+x = UpSampling1D(2)(x) #outvol=880x16
+decoded = Convolution1D(1,4, activation='sigmoid', border_mode='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='rmsprop', loss='mse')
+
+x_train = np.reshape(data_input, (data_input.shape[0], data_input.shape[1],1))
+print x_train.shape
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=20,
+                batch_size=100,
+                shuffle=True,
+                validation_data=(x_train, x_train),
+                callbacks=[])
+
+print "ok"
